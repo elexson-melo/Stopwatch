c78d8b377c7529e81c0df1b699ee35783529e890
elexson-melo <elexson.melo@gmail.com>
2022-04-04 11:10:36 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Threading;
+
+namespace Stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("S = Seconds => 10s = 10 seconds");
+            Console.WriteLine("M = Minutes => 1m = 1 minute");
+            Console.WriteLine("0 = Exit");
+            Console.WriteLine("How long do you want to count?");
+
+            string data = Console.ReadLine().ToLower();
+            char type = char.Parse(data.Substring(data.Length - 1, 1));
+            int time = int.Parse(data.Substring(0, data.Length - 1));
+            int multiplier = 1;
+
+            if(type == 'm')
+               multiplier = 60;
+            
+            if(time == 0)
+               System.Environment.Exit(0);
+
+            PreStart(time * multiplier);   
+
+        }
+        
+        static void PreStart(int time)
+        {
+            Console.Clear();
+            Console.WriteLine("Ready?..");
+            Thread.Sleep(1000);
+            Console.WriteLine("Set..");
+            Thread.Sleep(1000);
+            Console.WriteLine("Go!");
+            Thread.Sleep(2500);
+
+            Start(time);
+
+
+        }
+
+
+        static void Start(int time)
+        {
+            int currentTime = 0;
+
+            while (currentTime != time)
+            {
+                Console.Clear();
+                currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(1000);
+            }
+
+            Console.Clear();
+            Console.WriteLine("Stopwatch Finished");
+            Thread.Sleep(2500);
+            Menu();
+        }
+
+    }
+
+}
+
